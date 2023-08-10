03ef8ac7d805aa3618184c1aaa9cef060fbec27c
jojo <luizj1718@gmail.com>
2023-08-10 16:51:09 -0300

    fix: add cors

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ const corsOptions = {
 };
 
 app.use(function (req, res, next) {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
+
+app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
