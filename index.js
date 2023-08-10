e7fd5a7a534c65d25cc762c83f05b6dc719c24ba
jojo <luizj1718@gmail.com>
2023-08-10 10:50:30 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import kpisRoutes from "./routes/kpi.js";
+import KPI from "./modules/KPI.js";
+import productsRoutes from "./routes/product.js"
+import Transaction from "./modules/Transaction.js";
+import  Product  from "./modules/Product.js";
+import trasactionRoutes from "./routes/transaction.js"
+import { kpis, products, transactions } from "./data/data.js";
+
+dotenv.config();
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use("/kpi", kpisRoutes);
+app.use("/product", productsRoutes);
+app.use("/transaction", trasactionRoutes)
+
+const PORT = process.env.PORT || 9000;
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis)
+    // Product.insertMany(products)
+    // Transaction.insertMany(transactions)
+  })
+  .catch((error) => console.log(`${error} did not connect`));
