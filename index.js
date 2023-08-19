c19bf32cd12b8c0776f92eaa9a061beb53a594dc
jojo <luizj1718@gmail.com>
2023-08-19 15:06:11 -0300

    refactor: cors

--- index.js
+++ index.js
@@ -21,41 +21,30 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-
-app.use(function (req, res) {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-});
-
-app.use(cors({
-  origin:"https://pigpay.vercel.app/",
-  methods:["GET"],
-}));
+app.use(cors());
 
 
 app.use("/kpi", kpisRoutes);
 app.use("/product", productsRoutes);
 app.use("/transaction", trasactionRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 mongoose.Promise = global.Promise;
 
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(async () => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-    // await mongoose.connection.db.dropDatabase();
-    // KPI.insertMany(kpis)
-    // Product.insertMany(products)
-    // Transaction.insertMany(transactions)
-  })
-  .catch((error) => console.log(`${error} did not connect`));
+
+try{
+  await mongoose
+    .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(async () => {
+      app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+      // await mongoose.connection.db.dropDatabase();
+      // KPI.insertMany(kpis)
+      // Product.insertMany(products)
+      // Transaction.insertMany(transactions)
+    })
+} catch (error) {
+  console.error(`${error} did not connect`);
+}
